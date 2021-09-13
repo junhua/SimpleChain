526aa7a3fe200ccb6d3ce64ebb0f4f70316678a9
junhua <junhua.viva@gmail.com>
2021-09-13 23:53:04 +0800

    added hashing

--- simpleChain.js
+++ simpleChain.js
@@ -1,9 +1,11 @@
+const SHA256 = require('crypto-js/sha256')
+
 class Block {
     constructor(data) {
         this.height = 0
         this.body = data
         this.time = 0
-        this.previousblockhash = ""
+        this.previousBlockHash = ""
         this.hash = ""
     }
 }
@@ -11,9 +13,16 @@ class Block {
 class Blockchain {
     constructor() {
         this.chain = []
+        this.addBlock(new Block("First block in the chain = Genesis block"))
     }
 
     addBlock(newBlock) {
+        if (this.chain.length>0){
+            newBlock.previousBlockHash = this.chain[this.chain.length-1].hash
+        }
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString()
+        newBlock.height = this.chain.length
+        newBlock.time = new Date().getTime().toString().slice(0,-3)
         this.chain.push(newBlock)
     }
 }
