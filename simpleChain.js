e2e7c7233e817674f2a28e75beb7b1a74af12ba7
junhua <junhua.viva@gmail.com>
2021-09-13 23:19:38 +0800

    added chain

--- simpleChain.js
+++ simpleChain.js
@@ -1,9 +1,19 @@
 class Block {
     constructor(data) {
-        this.hash = ""
         this.height = 0
         this.body = data
         this.time = 0
         this.previousblockhash = ""
+        this.hash = ""
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = []
+    }
+
+    addBlock(newBlock) {
+        this.chain.push(newBlock)
     }
 }
